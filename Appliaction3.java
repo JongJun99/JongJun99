3306e9462ddce35450a9a67a9832a41508e74758
JongJun99 <157366488+JongJun99@users.noreply.github.com>
2024-01-26 17:58:06 +0900

    AND 연산과 OR 연산의 특징

---
+++ Appliaction3.java
@@ -0,0 +1,39 @@
+package com.ohgiraffers.section5.logical;
+
+public class Appliaction3 {
+
+    public static void main(String[] args) {
+
+        /*AND 연산과 OR 연산의 특징
+        * 논리식 && 논리식 : 앞의 결과가 false이면 뒤를 실행 안함
+        *  조건식 두개가 모두 만족해야 true를 반환하기 때문에 앞의 결과가 false가 나오면 뒤의 조건을 확인하지 않고 false를 반환한다.
+        *  뒤의 조건을 확인할 필요없이 false를 반환한다.
+        *   따라서 연산 횟수를 줄이기 위해서는 앞에 false가 나올 가능성이 높은 조건을 작성한다.
+        *
+        * 논리식 || 논리식 : 앞의 결과가 true이면 뒤를 실행 안함
+        *  조건식 하나만 만족해도 true를 반환하기 때문에 앞의 결과가 true가 나오면 뒤의 조건을 확인할 필요없이 true를 반환한다.
+        *  따라서 연산 횟수를 줄이기 위해서는 앞에 true가 나올 가능성이 높은 조건을 작성한다.
+        *  */
+
+        int num1 = 10;
+
+        int result1 = (false && ++num1 > 0)? num1 : num1; //++num1이 실행되면 11이지만 조건문에
+        System.out.println("결과값은 ? :" + result1); //false가 있기 때문에 실행을 안하고 10값이 출력.
+
+
+        int num2 = 10;
+        int result2 = (true || ++num2 > 0 )? num2 : num2; //++num2가 실행되면 11이고 true이지만
+        System.out.println("결과값은? :" + (result2)); // or 연산자는 true이면 뒤를 실행 안하기때문에 결과값 10
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
